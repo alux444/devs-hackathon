503bda5e17fbadec15bc911106b43553f12a8ea6
alux444 <aluux999@gmail.com>
2023-07-08 11:45:50 +1200

    aA

---
+++ src/components/loginpage/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login </h1>
+      <LoginForm />
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Login;
