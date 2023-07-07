4de7d19182ec09997f8f85db794342e01f46bdbe
alux444 <aluux999@gmail.com>
2023-07-08 11:48:28 +1200

    basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Login from "./components/loginpage/Login";
 
 function App() {
   return (
     <>
       <p className="border-2 text-[red]">Hello</p>
+      <Login />
     </>
   );
 }
---
+++ src/components/loginpage/SignUpForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUpForm = () => {
+  return (
+    <div>
+      <p>Signup</p>
+    </div>
+  );
+};
+
+export default SignUpForm;
