5d87b6c80c1734c039b7e090564b88a5b2d77f3c
alux444 <aluux999@gmail.com>
2023-07-08 23:12:09 +1200

    outside click on menu

--- src/components/mainpage/SideBar.jsx
+++ src/components/mainpage/SideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import CreatePostModal from "../posts/CreatePostModal";
 import { PageContext, UserContext } from "../../App";
 import AddWorkoutModal from "../posts/AddWorkoutModal";
@@ -10,6 +10,7 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import useOutsideClick from "../../utils/useOutsideClose";
 
 const SideBar = () => {
   const [openNewPost, setOpenNewPost] = useState(false);
@@ -18,6 +19,13 @@ const SideBar = () => {
   const [showMenu, setShowMenu] = useState(false); // Added state for menu visibility
   const { user, setUser } = useContext(UserContext);
   const { setPage } = useContext(PageContext);
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
+  const menuRef = useRef(null);
+  useOutsideClick(menuRef, closeMenu);
 
   const signOut = () => {
     setUser({ loggedIn: false, username: "", email: "" });
@@ -73,16 +81,13 @@ const SideBar = () => {
           <SettingsIcon />
         </button>
       </div>
-      <button
-        className="hidden lg:block"
-        onClick={() => setShowMenu(!showMenu)}
-      >
-        <MenuIcon />
-      </button>
 
       {showMenu && (
         <div className="lg:flex justify-center items-center absolute w-[88vw] animate-fade-in">
-          <div className="flex items-center w-[60%] border-[1px] justify-center border-[1px] p-2 bg-[rgba(0,0,0,0.9)] rounded-[20px]">
+          <div
+            className="flex items-center border-[1px] justify-center border-[1px] p-2 bg-[rgba(0,0,0,0.9)] rounded-[20px]"
+            ref={menuRef}
+          >
             <button onClick={() => setPage("home")}>
               <HomeIcon />
             </button>
@@ -102,12 +107,19 @@ const SideBar = () => {
           </div>
         </div>
       )}
-
-      {user.loggedIn && (
-        <div className="flex flex-col items-center">
-          <button onClick={() => signOut()}>Sign out</button>
-        </div>
-      )}
+      <div className="flex gap-1">
+        <button
+          className="hidden lg:block"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          <MenuIcon />
+        </button>
+        {user.loggedIn && (
+          <div className="flex flex-col items-center">
+            <button onClick={() => signOut()}>Sign out</button>
+          </div>
+        )}
+      </div>
       {openNewPost && (
         <CreatePostModal open={openNewPost} close={closeNewPost} />
       )}
