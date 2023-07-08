17dca003b5f0fc7f744f8ebb0d9d0f14dae7ff64
alux444 <aluux999@gmail.com>
2023-07-08 12:37:09 +1200

    cimut

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import Login from "./components/loginpage/Login";
 import Home from "./components/mainpage/Home";
 
 export const UserContext = createContext();
+export const PageContext = createContext();
 
 function App() {
   const [user, setUser] = useState({ loggedIn: false, email: "" });
+  const [page, setPage] = useState("home");
   return (
     <>
       <UserContext.Provider value={{ user, setUser }}>
-        <Login />
-        <Home />
+        <PageContext.Provider value={{ page, setPage }}>
+          <Login />
+          <Home />
+        </PageContext.Provider>
       </UserContext.Provider>
     </>
   );
---
+++ src/components/mainpage/SearchPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchPage = () => {
+  return (
+    <div>
+      <p>s</p>
+    </div>
+  );
+};
+
+export default SearchPage;
