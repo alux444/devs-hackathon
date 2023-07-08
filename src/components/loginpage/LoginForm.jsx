157f14da885b8f5294239aa67f3ecb5fc6f4a93e
JACK CHOI <jiyongboy@gmail.com>
2023-07-08 12:37:34 +1200

    test

--- src/components/loginpage/LoginForm.jsx
+++ src/components/loginpage/LoginForm.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import { useState } from "react";
+import useFirebase from "../../utils/useFirebase";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const { attemptLogin } = useFirebase();
+
+  async function onSubmit(event){
+    event.preventDefault();
+    setMessage("");
+    const loginSuccess = await attemptLogin(email, password);
+    if (!loginSuccess){
+      setMessage("Invalid login details");
+    }
+    // more stuff
+  }
+  
+  function handleEmailChange(event){
+    console.log(email);
+    setEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event){
+    console.log(password);
+    setPassword(event.target.value);
+  }
+
   return (
     <div>
-      <p>loginform</p>
+      <form onSubmit={onSubmit}>
+        <div >
+          <h3>
+            Email
+          </h3>
+          <input type="email" onChange={handleEmailChange}/>
+        </div>
+
+        <div >
+          <h3>
+            Password
+          </h3>
+          <input type="password" onChange={handlePasswordChange}/>
+        </div>
+        <button type="submit" >
+          Login
+        </button>
+        <h4>
+          {message}
+        </h4>
+      </form>
+
     </div>
   );
 };
--- src/components/loginpage/SignUpForm.jsx
+++ src/components/loginpage/SignUpForm.jsx
@@ -1,9 +1,70 @@
 import React from "react";
-
+import { useState } from "react";
+import useFirebase from "../../utils/useFirebase";
 const SignUpForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const { validateEmail, attemptLogin, createUser } = useFirebase();
+  const [message, setMessage] = useState("");
+  function handleEmailChange(event){
+    setEmail(event.target.value);
+  }
+  function handlePasswordChange(event){
+    setPassword(event.target.value);
+  }
+  function handleConfirmChange(event){
+    setConfirm(event.target.value);
+  }
+  async function onSubmit(event){
+    event.preventDefault();
+    if (password.length < 5){
+      setMessage("Password is too short");
+      return;
+    }
+    if (password !== confirm){
+      setMessage("Passwords to not match");
+      return;
+    }
+    const emailUsed = await validateEmail(email);
+    if (emailUsed){
+      setMessage("Email is in use.");
+      return;
+    }
+    await createUser(email, false, password);
+    const loginSuccess = await attemptLogin(email, password);
+  }
+
+
+
+
   return (
     <div>
-      <p>Signup</p>
+      <form onSubmit={onSubmit}>
+        <div>
+          <h3>
+            Email
+          </h3>
+          <input type="email" onChange={handleEmailChange} value={email}/>
+        </div>
+        <div>
+          <h3>
+            Password
+          </h3>
+          <input type="password" onChange={handlePasswordChange} value={password}/>
+        </div>
+        <div>
+          <h3>
+            Confirm Password
+          </h3>
+          <input type="password" onChange={handleConfirmChange} value={confirm}/>
+        </div>
+        <button type="submit">Sign up</button>
+        <h4>{message}</h4>
+
+      </form>
+
+
     </div>
   );
 };
