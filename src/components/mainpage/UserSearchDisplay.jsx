c90b5736bd90ae4535f1bb4d9040abea1599d5e9
alux444 <aluux999@gmail.com>
2023-07-08 22:07:31 +1200

    profile test

--- src/components/mainpage/UserSearchDisplay.jsx
+++ src/components/mainpage/UserSearchDisplay.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import Profile from "../portfoliopage/Profile";
 
 const UserSearchDisplay = ({ user }) => {
+  const [showProfile, setShowProfile] = useState(false);
+
+  const visitProfile = () => {
+    setShowProfile(!showProfile);
+  };
+
   return (
-    <div className="mt-2 flex gap-2 items-center">
+    <div className="mt-2 flex gap-2 items-center relative">
       <img
         src={
           user.avatar == ""
@@ -12,7 +19,12 @@ const UserSearchDisplay = ({ user }) => {
         className="w-[5vw] lg:w-[9vw]"
       />
       {user.username}
-      <button>Visit Profile</button>
+      <button onClick={() => setShowProfile(true)}>Visit Profile</button>
+      {showProfile && (
+        <div className="absolute">
+          <Profile user={user.email} />
+        </div>
+      )}
     </div>
   );
 };
